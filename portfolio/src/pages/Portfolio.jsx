257ca6ce083329ab2ccf88bd27985842ce7cfee5
Nic Miller <nicmiller13@gmail.com>
2024-11-13 19:15:54 -0500

    styled resume, contact, and added to resume

--- portfolio/src/pages/Portfolio.jsx
+++ portfolio/src/pages/Portfolio.jsx
@@ -7,12 +7,12 @@ import "../styles/portfolio.css";
 //make my own projects
 
 const projects = [
-    { title: "Weather Dashboard", image: 'src/assets/images.jpg', url:"https://github.com/MavMMSZ/weather-dashboard" },
-    { title: "Employee Tracker", image: 'src/assets/images.jpg', url:"https://github.com/MavMMSZ/employee-tracker" },
-    { title: "README Generator", image: 'src/assets/images.jpg', url:"https://github.com/MavMMSZ/readMeGenerator" },
-    { title: "Vacation Calculator", image: 'src/assets/images.jpg', url:"https://github.com/HarrisCH258/Vacation-Budget-Calculator" },
-    { title: "Vehicle Builder", image: 'src/assets/images.jpg', url:"https://github.com/MavMMSZ/vehicle-builder" },
-    { title: "React Portfolio", image: 'src/assets/images.jpg', url:"https://github.com/MavMMSZ/react-portfolio" },
+    { title: "Weather Dashboard", image: 'src/assets/weather.png', url:"https://github.com/NicMiller35/9-module-challenge" },
+    { title: "Employee Tracker", image: 'src/assets/tracker.png', url:"https://github.com/NicMiller35/10-module-challenge" },
+    { title: "README Generator", image: 'src/assets/readme.png', url:"https://github.com/NicMiller35/readme-challenge" },
+    { title: "SlackPacking", image: 'src/assets/slack2.png', url:"https://github.com/D-Wargle/First-Project" },
+    { title: "Vehicle Builder", image: 'src/assets/builder.png', url:"https://github.com/NicMiller35/8-module-challenge" },
+    { title: "React Portfolio", image: 'src/assets/portfolio.png', url:"https://github.com/NicMiller35/12-module-challenge" },
 ];
 
 export default function Portfolio() {
--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -1,17 +1,34 @@
+import "../styles/Resume.css";
+
+
 const Resume = () => {
-    return (
-      <section class="resume">
-        <h2>My Resume</h2>
-        <a href="/path-to-resume.pdf" download>Download Resume</a>
-        <ul>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-          {/* List other skills */}
-        </ul>
-      </section>
-    );
-  }
-  
-  export default Resume;
-  
+  return (
+    <section class="resume">
+      <h2 >My Resume</h2>
+      <a href="/path-to-resume.pdf" download>Download Resume</a>
+      <ul>
+        <h2 class='frontskill'>Front end devlopment skills</h2>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        {/* List other skills */}
+      </ul>
+      <h2>Back end development skills</h2>
+      <ul>
+        <li>Javascript</li>
+        <li>Node.js</li>
+        <li>Typescript</li>
+      </ul>
+      <h2>Other skills</h2>
+      <ul>
+        <li>Git</li>
+        <li>Debugging</li>
+        <li>Prompt generation</li>
+        <li>Problem solving</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Resume;
