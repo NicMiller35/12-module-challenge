635b937f3a059b8e9a591fe10d79a3123cf02980
Nic Nic Miller <nicmiller13@gmail.com>
2025-02-26 19:01:08 -0500

    updated resume page and added new pdf of resume

--- portfolio/src/pages/Resume.jsx
+++ portfolio/src/pages/Resume.jsx
@@ -5,13 +5,16 @@ const Resume = () => {
   return (
     <section class="resume">
       <h2 >My Resume</h2>
-      <a href="/assets/Resume.pdf" download>Download Resume</a>
+      <a href="/assets/SE-Resume.pdf" download>Download Resume</a>
       <ul>
         <h2 class='frontskill'>Front end devlopment skills</h2>
         <li>HTML</li>
         <li>CSS</li>
         <li>JavaScript</li>
         <li>React</li>
+        <li>Bootstrap</li>
+        
+
         
       </ul>
       <h2>Back end development skills</h2>
@@ -19,12 +22,15 @@ const Resume = () => {
         <li>Javascript</li>
         <li>Node.js</li>
         <li>Typescript</li>
+        <li>Express</li>
+        <li>PostgreSQL</li>
+        <li>MongoDB</li>
       </ul>
       <h2>Other skills</h2>
       <ul>
-        <li>Git</li>
+        <li>Git/Terminal</li>
         <li>Debugging</li>
-        <li>Prompt generation</li>
+        <li>Prompt Engineering</li>
         <li>Problem solving</li>
       </ul>
     </section>
